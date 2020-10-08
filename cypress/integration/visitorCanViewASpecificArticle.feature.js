691ff705fd9faedd0ff38ae6665e1dc0b4f217fd
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-10-08 19:18:55 +0200

    Creates test to check that a visitor can view a specific article. Adds content to articles object.

---
+++ cypress/integration/visitorCanViewASpecificArticle.feature.js
@@ -0,0 +1,36 @@
+describe("visitor can see a specific article", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:articles.json"
+    });
+
+    cy.visit("/");
+  });
+
+  it("visitor can see a specific article's title", () => {
+    cy.get("[data-cy='article-1']").within(() => {
+      cy.get("[data-cy='title']").should("contain", "Hello World");
+    });
+  });
+
+  it("visitor can see a specific article's teaser", () => {
+    cy.get("[data-cy='article-2']").within(() => {
+      cy.get("[data-cy='teaser']").should(
+        "contain",
+        "Sun is always shining in Kista"
+      );
+    });
+  });
+
+  it("visitor can see a specific article's content", () => {
+    cy.get("[data-cy='article-3']").within(() => {
+      cy.get("[data-cy='content']").should(
+        "contain",
+        "Jenny is cool"
+      );
+    });
+  });
+});
