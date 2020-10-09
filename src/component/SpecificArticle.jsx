8befd6a6bc1d2b646c25100466170872ea9bfcbe
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-09 13:47:34 +0200

    Adding styling to show_index, center the text

--- src/component/SpecificArticle.jsx
+++ src/component/SpecificArticle.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Articles from "../modules/articles";
+import { Container } from "semantic-ui-react";
 
 const SpecificArticle = () => {
   const [article, setArticle] = useState({});
@@ -16,11 +17,13 @@ const SpecificArticle = () => {
   }, [id]);
 
   return (
+    <Container textAlign="center">
     <div data-cy="article">
       <h1 data-cy="title">{article.title}</h1>
       <h3 data-cy="teaser">{article.teaser}</h3>
       <p data-cy="content">{article.content}</p>
     </div>
+    </Container>
   );
 };
 
