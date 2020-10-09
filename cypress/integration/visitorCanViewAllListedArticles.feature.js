e4b8c4f2017eb284f03f7605394cba4b818dbe14
oliverochman <oliver.ochman@hotmail.com>
2020-10-09 09:35:34 +0200

    modified tests

--- cypress/integration/visitorCanViewAllListedArticles.feature.js
+++ cypress/integration/visitorCanViewAllListedArticles.feature.js
@@ -1,12 +1,11 @@
-describe("visitor can see all listed articles", () => {
+describe("Visitor can see all listed articles", () => {
   beforeEach(() => {
     cy.server();
     cy.route({
       method: "GET",
       url: "http://localhost:3000/api/v1/articles",
-      response: "fixture:articles.json",
+      response: "fixture:articles_index.json",
     });
-
     cy.visit("/");
   });
 
