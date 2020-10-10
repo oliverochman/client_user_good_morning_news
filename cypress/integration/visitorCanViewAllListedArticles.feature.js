e65724722a15e8a3c6ae8a6f0ac492b513e6031c
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-10 18:32:24 +0200

    Following the test structure of the backend by modifying visitorCanViewAllListedArticles with adding tests for each seperate category in a corresponding context, one context for each category.

--- cypress/integration/visitorCanViewAllListedArticles.feature.js
+++ cypress/integration/visitorCanViewAllListedArticles.feature.js
@@ -1,27 +1,133 @@
 describe("visitor can see all listed articles", () => {
-  beforeEach(() => {
-    cy.server();
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/v1/articles",
-      response: "fixture:articles_index.json",
+  context("when visiting starting page", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles",
+        response: "fixture:articles_index.json",
+      });
+
+      cy.visit("/");
     });
 
-    cy.visit("/");
-  });
+    it("visitor can see article title", () => {
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.get("[data-cy='title']").should("contain", "Hello World");
+      });
+    });
 
-  it("visitor can see article title", () => {
-    cy.get("[data-cy='article-1']").within(() => {
-      cy.get("[data-cy='title']").should("contain", "Hello World");
+    it("visitor can see article teaser", () => {
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.get("[data-cy='teaser']").should(
+          "contain",
+          "Sun is always shining in Kista"
+        );
+      });
     });
   });
 
-  it("visitor can see article teaser", () => {
-    cy.get("[data-cy='article-2']").within(() => {
-      cy.get("[data-cy='teaser']").should(
-        "contain",
-        "Sun is always shining in Kista"
-      );
+  context("when visiting sports category", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/sports",
+        response: "fixture:sports_index.json",
+      });
+      cy.visit("/");
+    });
+    it("visitor can see article title", () => {
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.get("[data-cy='title']").should("contain", "Hello World");
+      });
+    });
+
+    it("visitor can see article teaser", () => {
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.get("[data-cy='teaser']").should(
+          "contain",
+          "Sun is always shining in Kista"
+        );
+      });
+    });
+  });
+
+  context("when visiting entertainment category", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/entertainment",
+        response: "fixture:entertainment_index.json",
+      });
+      cy.visit("/");
+    });
+    it("visitor can see article title", () => {
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.get("[data-cy='title']").should("contain", "Hello World");
+      });
+    });
+
+    it("visitor can see article teaser", () => {
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.get("[data-cy='teaser']").should(
+          "contain",
+          "Sun is always shining in Kista"
+        );
+      });
+    });
+  });
+
+  context("when visiting weather category", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/weather",
+        response: "fixture:weather_index.json",
+      });
+      cy.visit("/");
+    });
+    it("visitor can see article title", () => {
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.get("[data-cy='title']").should("contain", "Hello World");
+      });
+    });
+
+    it("visitor can see article teaser", () => {
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.get("[data-cy='teaser']").should(
+          "contain",
+          "Sun is always shining in Kista"
+        );
+      });
+    });
+  });
+
+  context("when visiting business category", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/business",
+        response: "fixture:business_index.json",
+      });
+      cy.visit("/");
+    });
+    it("visitor can see article title", () => {
+      cy.get("[data-cy='article-1']").within(() => {
+        cy.get("[data-cy='title']").should("contain", "Hello World");
+      });
+    });
+
+    it("visitor can see article teaser", () => {
+      cy.get("[data-cy='article-2']").within(() => {
+        cy.get("[data-cy='teaser']").should(
+          "contain",
+          "Sun is always shining in Kista"
+        );
+      });
     });
   });
 });
