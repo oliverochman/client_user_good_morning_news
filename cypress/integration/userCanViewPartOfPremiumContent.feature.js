0a2740c7c030d4a1f291687703b6a570e81c8358
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-16 12:10:13 +0200

    Creating feature test for user can view part of premium content, and fixture file article_show_premium

---
+++ cypress/integration/userCanViewPartOfPremiumContent.feature.js
@@ -0,0 +1,40 @@
+describe("Premium article view for visitor/user", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:articles_index.json",
+    });
+  });
+  context("when current user role is not subscriber", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/1",
+        response: "fixture:article_show_premium.json",
+      });
+      cy.visit("/");
+      cy.get("[data-cy='article-1']").click();
+    });
+    it("can only see first 300 characters", () => {
+      cy.get("[data-cy='title']").should(
+        "contain",
+        "Hello World"
+      );
+      cy.get("[data-cy='content']").should(
+        "have.text",
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."
+      );
+      cy.get("[data-cy='premium-message']").should(
+        "contain",
+        "This is part of our premium content, to get full access become premium user"
+      );
+      cy.get("[data-cy='subscription-button']").should(
+        "contain",
+        "Become premium!"
+      );
+    });
+  });
+});
